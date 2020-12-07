a14eda254d659d3cbc8ba22d19d10cca4123e238
jprx <josravix@gmail.com>
2020-12-06 18:03:02 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# pretty-secure-processor
+A RISC-V core with a couple of security features. Part of my "Super Duper Secure SoC" project.
---
+++ hdl/psp.sv
@@ -0,0 +1,21 @@
+/*
+ * Joseph Ravichandran
+ * December 6, 2020
+ */
+
+/*
+ * psp
+ * A pretty secure processor.
+ */
+module psp
+    (
+        input logic clk
+    );
+
+    // Main Memory
+
+    // Caches
+
+    // Core
+
+endmodule
